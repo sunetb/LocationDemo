ff5fe3ee566406369466903d81c1fb014aba249f
sunetb <sunetb@gmail.com>
2023-03-15 00:42:03 +0100

    Added more UI and stop. Overall simplified

--- app/src/main/java/com/example/locationdemo/MainActivity.java
+++ app/src/main/java/com/example/locationdemo/MainActivity.java
@@ -1,25 +1,22 @@
 package com.example.locationdemo;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.location.LocationManagerCompat;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -36,89 +33,110 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.OnTokenCanceledListener;
 import com.google.android.gms.tasks.Task;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     FusedLocationProviderClient fusedLocationClient;
-    //boolean justAccepted = false;
+    LocationCallback locationCallback; //for continuous updates
 
     TextView text;
+    Button active, stop;
+    RadioButton once, continuous;
     int counter = 0;
-
-    int LOCATION_TYPE = 1;  //1: Ask once
-    //2: Ask until cancelled
-
-    LocationCallback locationCallback; //for continuous updates
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setting the views
         text = findViewById(R.id.statustext);
-
-
-        //Did user already accept the needed permissions?
-        boolean fineLocationAlreadyAccepted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-        boolean coarseLocationAlreadyAccepted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-
-        //If we dont have the permissions, launch a dialogue that asks the user to accept
-        askToEnable();
-        if (!(fineLocationAlreadyAccepted && coarseLocationAlreadyAccepted)) {
-            checkPermissions();
-
-        }
-        else if (LOCATION_TYPE == 1) askForLocationOnce();
-        else askForLocationContinuously();
+        active = findViewById(R.id.button);
+        stop = findViewById(R.id.button2);
+        stop.setOnClickListener(this);
+        active.setOnClickListener(this);
+        once = findViewById(R.id.once);
+        continuous = findViewById(R.id.continuous);
 
         //startMaps();
     }
 
-    private void askForLocationContinuously() {
+    @Override
+    public void onClick(View view) {
+        if (view == stop){
+            if (locationCallback != null) {
+                fusedLocationClient.removeLocationUpdates(locationCallback);
+            }
+            return;
+        }
+
+        //Check app permissions
+        boolean fineLocationAlreadyAccepted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        boolean coarseLocationAlreadyAccepted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        if (!(fineLocationAlreadyAccepted && coarseLocationAlreadyAccepted)) {
+            //Dialogue to ask the user
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1234);
+            text.setText("Press again");
+            return;
+
+        }
+
+        //We ask if Location is enabled in settings
+        askToEnableLocation();
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        text.setText("");
+
+        //Check the radiobuttons
+        if (once.isChecked()) askForLocationOnce();
+        else askForLocationContinuously();
+
+    }
+
+    private void askForLocationContinuously() {
+
+        // Instantiate the callback-object
         locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 counter++;
                 if (locationResult == null) {
-                    text.setText("Null location or error");
-                    askToEnable();
+                    text.setText("Error: no location detected");
                     return;
                 }
-                for (Location location : locationResult.getLocations()) {
-                    text.setText(counter + " location " + location);
-                }
+                Location location = locationResult.getLastLocation();
+                System.out.println(location);
+                String result = "Long: " + location.getLongitude() + " | Lat: " + location.getLatitude();
+                text.setText(counter + " location " + result);
             }
         };
+
+        //Create a request object
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        //Required extra check before calling requestLocationUpdates()
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-               return;
+            return;
         }
-        fusedLocationClient.requestLocationUpdates(newRequest(),
+
+        //Ask for updates
+        fusedLocationClient.requestLocationUpdates(locationRequest,
                 locationCallback,
                 Looper.getMainLooper());
 
     }
     private void askForLocationOnce() {
-        System.out.println("Start measuring");
-
-        //// Now that we have the right permissions and GPS is enabled, we can ask for locations
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
+        //Required extra check before calling getCurrentLocation()
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            System.out.println("permission check failed");
             return;
         }
         fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, new CancellationToken() {
             @NonNull
             @Override
-            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
-                return null;
-            }
-
+            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {return null;}
             @Override
             public boolean isCancellationRequested() {
                 return false;
@@ -130,33 +148,22 @@ public class MainActivity extends AppCompatActivity {
                     String status = counter + " location: " + location.toString();
                     text.setText(status);
                 } else {
-                    text.setText("Null location or error");
-                    askToEnable();
+                    text.setText("Error: No location");
+                    askToEnableLocation();
 
                 }
             }
         });
-
-
-
-
     }
 
 
     //Helper method
-    LocationRequest newRequest() {
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(1000);
-        locationRequest.setFastestInterval(500);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        return locationRequest;
-    }
-
     //If the user disabled GPS location on their phone, we ask them to turn it on
-    void askToEnable() {
-        LocationRequest l = newRequest();
+    void askToEnableLocation() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(l);
+                .addLocationRequest(locationRequest);
 
         SettingsClient client = LocationServices.getSettingsClient(this);
         Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
@@ -164,13 +171,12 @@ public class MainActivity extends AppCompatActivity {
         task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
             @Override
             public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                // All location settings are satisfied. The client can initialize
+                // User has Location enabled
             }
         });
-
+        //If GPS is diabled, we show a dialog
         task.addOnFailureListener(this, new OnFailureListener() {
             int REQUEST_CHECK_SETTINGS = 0x1;
-
             @Override
             public void onFailure(@NonNull Exception e) {
                 if (e instanceof ResolvableApiException) {
@@ -178,11 +184,8 @@ public class MainActivity extends AppCompatActivity {
                     // by showing the user a dialog.
                     try {
                         // Show the dialog by calling startResolutionForResult(),
-                        // and check the result in onActivityResult().
                         ResolvableApiException resolvable = (ResolvableApiException) e;
-
-                        resolvable.startResolutionForResult(MainActivity.this,
-                                REQUEST_CHECK_SETTINGS);
+                        resolvable.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
                     } catch (IntentSender.SendIntentException sendEx) {
                         // Ignore the error.
                     }
@@ -192,44 +195,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //Check app permissions on runtime if not accepted earlier or if revoked
-    private void checkPermissions() {
-        // 1: Build the Permission request object with callbacks
-        ActivityResultLauncher<String[]> locationPermissionRequest =
-                registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
-                            System.out.println("Result recieved");
-                    Boolean fineLocationGranted = null;
-                    Boolean coarseLocationGranted = null;
-                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                        fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
-                        coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
-                    }
-                    else{
-                        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-                        fineLocationGranted = LocationManagerCompat.isLocationEnabled(locationManager);
-                        coarseLocationGranted = LocationManagerCompat.isLocationEnabled(locationManager);
-                    }
-
-                            if (fineLocationGranted != null && fineLocationGranted) {
-                                System.out.println("Fine accepted");
-                                // Spaghetti solution. Could be handled better
-                                if (LOCATION_TYPE == 1) askForLocationOnce();
-                                else askForLocationContinuously();
-                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
-                                System.out.println("coarse accepted");
-                            } else {
-                                System.out.println("None accepted");
-                            }
-                        }
-                );
-        // 2: Launch the permission request dialogue
-        locationPermissionRequest.launch(new String[]{
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-        });
-
-    }
-
+//Just for fun
 
     private void startMaps() {
 
@@ -245,5 +211,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(mapIntent);
 
     }
+
 
 }
